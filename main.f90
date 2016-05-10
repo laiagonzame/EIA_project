c4e04f22203b628f313be7d6f10cf5a27edf296f
Miquel <marchenaangos.miquel@gmail.com>
2016-05-10 13:28:17 +0200

    primera versio de main

---
+++ main.f90
@@ -0,0 +1,53 @@
+program van_der_waals
+implicit none
+! N: number of time steps
+! M: number of atoms
+! boxL: boxlength
+! mass: particle mass
+! rho: density
+! kBTref: reference temperature
+! nf: degrees of freedom
+! ecin: kinetic energy
+! epot: potential energy
+! temp: temperature
+
+! system parameters
+integer :: M, nf
+double precision :: boxL, mass, kBTref
+double precision :: ecin, epot, temp
+! Integration parameters
+integer :: i, j, N
+double precision :: dt
+! General variables
+double precision, dimension(:,:), allocatable :: r, v, accel
+! defining parameters
+parameter(M = 400, N = 100)
+parameter(mass = 4.)
+
+! Defining dimensions
+allocate(r(3,M), v(3,M), accel(3,M), rho(P))
+
+! Init variables
+kBTref = 2.
+dt = 1. / 500
+nf = 3 * M - 3 ! number of degrees of freedom 
+
+! Initial configuration
+
+! Open files
+open(unit=2, file='../data/energy-temp.data', status='unknown')
+
+! Temporal loop
+do i = 1, N
+   ! Update forces
+
+   ! Update r and v
+
+   ! Compute magnitdes
+
+   ! Save temporal serie 
+
+end do
+
+close(2)
+end program
