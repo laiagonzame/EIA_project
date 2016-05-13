46abd1c1a4c86315bf565941555395e3424e61f6
Miquel <marchenaangos.miquel@gmail.com>
2016-05-13 10:46:56 +0200

    dimensions matrius, noms variables i 2 comentaris

--- temperature.f90
+++ temperature.f90
@@ -4,40 +4,40 @@ public
 
 ! Modul que calcula la temperatura del sistema, kbt
 ! Com a input accepta:
-! el número de particulas de la dinamica (N)¡
-! una matriu (N,3) amb les velocitats de las N particulas (vel)
+! el número de particulas de la dinamica (M)¡
+! una matriu (3,M) amb les velocitats de las N particulas (vel)
 ! el paso de tiempo de la dinamica (dt)
-! la massa de las particulas (m)
+! la massa de las particulas (mass)
 ! Retorna la velocitat mitja de les partícules i la temperatura  (kbt)
-
-
 
 contains
 
-subroutine compute_temperature(kbt,vel,N,dt,m)
- real*8, dimension(3,N),intent(in) :: vel
- integer, intent(in) :: N
- real*8, intent(in) :: dt,m
- integer :: i
- real*8, intent(inout) :: kbt
+subroutine compute_temperature(kbt ,vel, M, dt, mass)
+integer, intent(in) :: M
+real*8, dimension(3,M),intent(in) :: vel
+real*8, intent(in) :: dt, mass
+real*8, intent(out) :: kbt
+integer :: i
 
- kbt=0.;
+kbt=0.
  
- do i=1,N
+do i=1,M
+! 1. Falta dividir pel numero de graus de llibertat: al main se
+! li diu nf ---> nf = 3*M-3 
+! 2. Si treballem en unitats de massa no caldria multiplicar-la aqui
+ kbt = kbt + mass*(vel(1,i)**2+vel(2,i)**2+vel(3,i)**2)/3.
 
-  kbt=kbt+m*(vel(1,i)**2+vel(2,i)**2+vel(3,i)**2)/3.
-
- end do
+end do
 
 
 
 end subroutine
 
-subroutine kinetic_energy(kine,kbt)
- real*8, intent(in) :: kbt
- real*8, intent(out) :: kine
+subroutine kinetic_energy(kine, kbt)
+real*8, intent(in) :: kbt
+real*8, intent(out) :: kine
 
- kine=3.*kbt/2.;
+kine=3.* kb t/ 2.
 
 end subroutine
 
