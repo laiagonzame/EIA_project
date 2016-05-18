2c9c3ffdc297832c552011a75504389053ae0576
Miquel <marchenaangos.miquel@gmail.com>
2016-05-18 11:06:57 +0200

    Arrel quadrada a la temperatura

--- ini.f90
+++ ini.f90
@@ -15,27 +15,27 @@ contains
 
 !**************************************************************
 subroutine inirandom(N,pos,vel,box,T)
-        !Subrutina que inicializa las particulas en la caja de manera
-        ! aleatoria en posicion y velocidad
+!Subrutina que inicializa las particulas en la caja de manera
+! aleatoria en posicion y velocidad
 
-        integer :: N,i,j,seed
-        real*8,dimension(3,N) ::pos,vel
-        real*8 :: box,aux,T
+integer :: N,i,j,seed
+real*8,dimension(3,N) ::pos,vel
+real*8 :: box,aux,T
 
-        !Inicializacion de los numeros aleatorios
-        seed=1221
-        call random_seed(seed)
+!Inicializacion de los numeros aleatorios
+seed=1221
+call random_seed(seed)
 
-        do j=1,N
-                do i=1,3
-                  !Posiciones
-                  call random_number(aux)
-                  pos(i,j)=(2.*aux-1.)*box
-                  !Velocidades
-                  call random_number(aux)
-                  vel(i,j)=(2.*aux-1.)*T 
-                enddo
-        enddo
+do j=1,N
+   do i=1,3
+      ! Posiciones
+      call random_number(aux)
+      pos(i,j)=(2.*aux-1.)*box
+      ! Velocidades
+      call random_number(aux)
+      vel(i,j)=(2.*aux-1.)*dsqrt(T) 
+   enddo
+enddo
 
 end subroutine
 
