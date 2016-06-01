ade36bf972ba30b14a3a441ad9fcd6f81ddc05ac
Miquel Marchena Angos <miquel@localhost.localdomain>
2016-06-01 23:27:28 +0200

    modified:   To_Do.txt 	deleted:    analysis/.plot_gr.py.swp 	modified:   analysis/plot_gr.py 	new file:   analysis/statistics.cpython-35m-x86_64-linux-gnu.so

--- analysis/plot_gr.py
+++ analysis/plot_gr.py
@@ -7,21 +7,38 @@ get_ipython().system('f2py3 -c ../statistics.f90 -m statistics')
 # import module
 import statistics as stat
 
-stat.declarate_radial_dist(L,total_esp,dr,g)
+# TODO import parameters
+boxL = 
+nhis = 
+N = number of snapshots
+M = 
 
-# import data
-r, gr = np.loadtxt("../data/gr.data").T
+rho = M / boxL**3
 
-fig = plt.figure(figsize=(3,3))
-plt.plot(r[:-1], gr[:-1])
+# TODO import trajectory
 
-x1, x2, y1, y2 = plt.axis()
-# plt.axis([x1, 4, y1, y2])
-# plt.locator_params('x', nbins=6, tight='True')
-plt.xlabel(r"r [$\sigma$]")
-plt.ylabel("g(r)")
-plt.legend(prop={'size':10})
-fig.tight_layout() 
-# plt.savefig("../plots/radial_dist.pdf")
+dr, gr = stat.statistics.declarate_radial_dist(boxL, nhis)
 
-plt.show()
+# accumulate statistics 
+for i in N:
+    gi = stat.statistics.accumulate_radial_dist(M, boxL, r[:,i], nhis, dr)
+    gr += gi / float(N) 
+
+# TODO n_acc
+gr = compute_radial_dist(nhis, dr, n_acc, rho)
+r = dr * np.arange(nhis)
+
+# 
+# fig = plt.figure(figsize=(3,3))
+# plt.plot(r[:-1], gr[:-1])
+# 
+# x1, x2, y1, y2 = plt.axis()
+# # plt.axis([x1, 4, y1, y2])
+# # plt.locator_params('x', nbins=6, tight='True')
+# plt.xlabel(r"r [$\sigma$]")
+# plt.ylabel("g(r)")
+# plt.legend(prop={'size':10})
+# fig.tight_layout() 
+# # plt.savefig("../plots/radial_dist.pdf")
+# 
+# plt.show()
