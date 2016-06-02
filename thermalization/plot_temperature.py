88898f1f97221a79d04e77aa3757e279727b273b
laiazean <gonzalezme.laia@gmail.com>
2016-06-02 11:11:19 +0200

    Creat main_trajs.f90, que només guarda les posicions i velocitats després de la termalització. Creada carpeta thermalization, amb un parell de gràfics de l'evolució de la temperatura i l l'energia per veure en quin moment termalitza

---
+++ thermalization/plot_temperature.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# IMPORTING DATA
+# TODO check the outputs files and cd to the data folder
+# TODO check time and temperature units
+# TODO check where we are saving figures
+t, temp = np.loadtxt('../data/temp.data').T
+
+plt.figure()
+plt.plot(t, temp)
+plt.xlabel('Time')
+plt.ylabel('Temperature')
+# plt.savefig('plots/temperature.pdf')
+plt.show()
---
+++ write_vmd.f90
@@ -0,0 +1,25 @@
+module vmd
+implicit none
+
+contains
+
+subroutine output(M,i,dt,r)
+!Subrutina que me guarda las coordenadas para que las pueda ver con el vmd
+integer, intent(in) :: M
+double precision, dimension(3,M), intent(in) :: r
+double precision, intent(in) :: dt
+integer :: i
+integer :: j
+
+
+write(20,*) M
+write(20,*) "trajectoria,iter:",i,"temps",i*dt,"ns"
+
+   do j=1,M
+   Write(20,*) "C ", r(1,j), r(2,j), r(3,j)
+   enddo
+
+end subroutine
+
+end module
+
