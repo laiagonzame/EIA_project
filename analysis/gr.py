d22e8c44afe9c6566d090a6f89168a5541972cb7
Miquel Marchena Angos <miquel@localhost.localdomain>
2016-06-07 16:39:26 +0200

    canvis a la g(r)

--- analysis/gr.py
+++ analysis/gr.py
@@ -2,49 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # compile RDF file into fortran, and create a module
-get_ipython().system('f2py3 -c ../statistics.f90 -m statistics')
+get_ipython().system('f2py3 -c -m statistics ../statistics.f90')
 
 # import module
 import statistics as stat
 
-# TODO import parameters
-boxL = 10
-nhis = 200
-N = 5
-M = 400
+# import parameters
+params = open('../data/params.data', 'r')
+boxL, nhis, M, sigma, epsil, mass, dt = params.readline().split()
+N = int(params.readline())
+M = int(M)
+boxL = float(boxL)
+nhis = int(nhis)
 
 rho = M / boxL**3
 
-# test
-r = np.array(np.random.randn(3,M,N), order='FORTRAN')
-#r = np.array(np.random.randn(3,M,N), order='C')
+# import trajectory
+r = np.loadtxt('../data/posicions.data').T
+r = np.reshape(r.T, (N, M, 3))
 
-# TODO import trajectory
-
+# initialize g(r)
 dr, gr = stat.statistics.declarate_radial_dist(boxL, nhis)
 
 # accumulate statistics 
 for i in range(N):
-    #gi = np.zeros(nhis)
-    #stat.statistics.accumulate_radial_dist(M, boxL, r[:,:,i], nhis, dr, gi)
-
-    gi = stat.statistics.accumulate_radial_dist(M, boxL, r[:,:,i], nhis, dr)
+    gi = stat.statistics.accumulate_radial_dist(M, boxL, r[i,:,:].T, nhis, dr)
     gr += gi
 
-gr = compute_radial_dist(nhis, dr, N, rho)
-r = dr * np.arange(nhis)
+gg = stat.statistics.compute_radial_dist(nhis, dr, N, rho, M, gr)
+rhis = dr * np.arange(nhis)
 
-# 
-# fig = plt.figure(figsize=(3,3))
-# plt.plot(r[:-1], gr[:-1])
-# 
-# x1, x2, y1, y2 = plt.axis()
-# # plt.axis([x1, 4, y1, y2])
-# # plt.locator_params('x', nbins=6, tight='True')
-# plt.xlabel(r"r [$\sigma$]")
-# plt.ylabel("g(r)")
-# plt.legend(prop={'size':10})
-# fig.tight_layout() 
-# # plt.savefig("../plots/radial_dist.pdf")
-# 
-# plt.show()
+
+fig = plt.figure(figsize=(3,3))
+plt.plot(rhis[:-1], gg[:-1])
+
+x1, x2, y1, y2 = plt.axis()
+# plt.axis([x1, 4, y1, y2])
+# plt.locator_params('x', nbins=6, tight='True')
+plt.xlabel(r"r [$\sigma$]")
+plt.ylabel("g(r)")
+plt.legend(prop={'size':10})
+fig.tight_layout() 
+# plt.savefig("../plots/radial_dist.pdf")
+
+plt.show()
