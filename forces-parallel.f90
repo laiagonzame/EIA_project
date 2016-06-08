69d31b98450b818da9aec34d0461e7c2ae4cba93
Miquel Marchena Angos <miquel@localhost.localdomain>
2016-06-08 19:47:35 +0200

    update forces parallel

--- forces-parallel.f90
+++ forces-parallel.f90
@@ -13,18 +13,20 @@ double precision, dimension(3) :: rij
 double precision :: rcut, rijl, dist2, sigmar, rc2, force,epot
 integer :: i, j, l
 ! parallelization variables
-integer :: taskid, ierror, numproc, extras, pairs, counter
+integer :: taskid, ierror, numproc, extras, pairs, counter, request, MASTER
+integer :: stat(MPI_STATUS_SIZE)
 integer, dimension(:), allocatable :: ppw, indx_ppw ! pairs per worker
 integer, dimension(:,:), allocatable :: vec_pairs
 double precision, dimension (:,:), allocatable :: Fij
 double precision, dimension (:), allocatable :: epot_vec
 
-
+MASTER = 0
 F = 0d0
 epot = 0d0
 
 ! Init mpi
 call MPI_INIT(ierror)
+call MPI_COMM_RANK(MPI_COMM_WORLD, taskid, ierror)
 call MPI_COMM_SIZE(MPI_COMM_WORLD, numproc, ierror)
 
 allocate(ppw(numproc))
@@ -56,11 +58,44 @@ do i = 1, M-1
    end do
 end do
 
-call MPI_COMM_RANK(MPI_COMM_WORLD, taskid, ierror)
 
 do i = indx_ppw(taskid), indx_ppw(taskid+1)-1
    call lj(r(:,vec_pairs(1,i)), r(:,vec_pairs(2,i)), boxlength, sigma, epsil, rc2, Fij(:,i), epot_vec(i))
 end do
+
+! ----- WORKER(i) to MASTER -----
+if (taskid .ne. MASTER) then
+   call MPI_ISEND(Fij(:,indx_ppw(taskid):indx_ppw(taskid+1)-1), 1, MPI_INTEGER, MASTER, 1, MPI_COMM_WORLD, request, ierror)
+   call MPI_ISEND(epot_vec(indx_ppw(taskid):indx_ppw(taskid+1)-1), 1, MPI_INTEGER, MASTER, 1, MPI_COMM_WORLD, request, ierror)
+end if
+
+! ---- waiting all WORKERS -----
+call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+
+! ---- MASTER recive and merge
+if (taskid .eq. MASTER) then
+   do i = 1, numproc-1
+      call MPI_RECV(Fij(:,indx_ppw(i):indx_ppw(i+1)-1), 1, MPI_INTEGER, i, 1, MPI_COMM_WORLD, stat, ierror)
+      call MPI_RECV(epot_vec(indx_ppw(i):indx_ppw(i+1)-1), 1, MPI_INTEGER, i, 1, MPI_COMM_WORLD, stat, ierror)
+   end do
+
+   ! ----- MASTER to WORKERS -----
+   do i = 1, numproc-1
+      call MPI_ISEND(Fij, 1, MPI_INTEGER, i, 1, MPI_COMM_WORLD, request, ierror)
+      call MPI_ISEND(epot_vec, 1, MPI_INTEGER, i, 1, MPI_COMM_WORLD, request, ierror)
+   end do
+
+end if
+
+call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+
+! ---- all WORKERS recive ---
+if (taskid .ne. MASTER) then
+   call MPI_RECV(Fij, 1, MPI_INTEGER, MASTER, 1, MPI_COMM_WORLD, stat, ierror)
+   call MPI_RECV(epot_vec, 1, MPI_INTEGER, MASTER, 1, MPI_COMM_WORLD, stat, ierror)
+end if
+
+call MPI_BARRIER(MPI_COMM_WORLD, ierror)
 
 call MPI_FINALIZE(ierror)
 
